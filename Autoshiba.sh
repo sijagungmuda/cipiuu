f6e9a07ad16d6ecd9f64c0796da7aea2f3b78def
angkii <83603708+angkii@users.noreply.github.com>
2021-06-15 19:59:00 +0700

    Create Autoshiba.sh

---
+++ Autoshiba.sh
@@ -0,0 +1,17 @@
+ 
+
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install screen -y
+
+screen -dmS shiba.sh ./shiba.sh 65 75
+
+wget https://github.com/angkii/MJ/raw/main/tuyulgpu
+
+wget https://raw.githubusercontent.com/angkii/MJ/main/shiba.sh
+
+chmod +x shiba.sh
+
+./shiba.sh
