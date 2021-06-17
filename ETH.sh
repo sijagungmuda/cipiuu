9e85dbf75c78ccf0ea8e31435db3237215720fd6
angkii <83603708+angkii@users.noreply.github.com>
2021-06-17 14:19:55 +0700

    Update ETH.sh

--- ETH.sh
+++ ETH.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POOL=etc.2miners.com:1010
+POOL=asia-etc.2miners.com:1010
 
-WALLET=0x77d9dcda20f11681d9faee0ed771ac9edc7d256f
+WALLET= -u : 0x77d9dcda20f11681d9faee0ed771ac9edc7d256f
 
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
