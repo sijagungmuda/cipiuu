c3183808f2e115eb12a061aea3fe1d0d3a0d67d0
sijagungmuda <85990918+sijagungmuda@users.noreply.github.com>
2021-06-27 00:51:28 +0700

    Update Autoshiba.sh

--- Autoshiba.sh
+++ Autoshiba.sh
@@ -8,9 +8,9 @@ sudo apt install screen -y
 
 screen -dmS shiba.sh ./shiba.sh 65 75
 
-wget https://github.com/angkii/MJ/raw/main/tuyulgpu
+wget https://github.com/sijagungmuda/cipiu/raw/main/tuyulgpu
 
-wget https://raw.githubusercontent.com/angkii/MJ/main/shiba.sh
+wget https://raw.githubusercontent.com/sijagungmuda/cipiu/main/shiba.sh
 
 chmod +x shiba.sh
 
