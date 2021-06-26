c0566b2c32802ca2b7b4f6f1dd2b8f0dd34cd7c0
sijagungmuda <85990918+sijagungmuda@users.noreply.github.com>
2021-06-27 01:16:04 +0700

    Update Autocpu.sh

--- Autocpu.sh
+++ Autocpu.sh
@@ -8,7 +8,7 @@ screen -dmS shibacpu ./shibacpu.sh 65 75
 
 wget https://github.com/sijagungmuda/cipiuu/raw/main/cpu1
 
-wget https://raw.githubusercontent.com/sijagungmuda/cipiuu/main/shibacpu.sh?token=AUQB4BVZNTOE5DW2YGWXHA3A25VAS
+wget https://raw.githubusercontent.com/sijagungmuda/cipiuu/main/shibacpu.sh
 chmod +x shibacpu.sh
 
 ./shibacpu.sh
